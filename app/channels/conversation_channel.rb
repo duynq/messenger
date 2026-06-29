5c425e80b5112696b9a5fc12fafe086a8bb1f87e
quang duy <duynq@users.noreply.github.com>
2026-06-29 20:42:55 +0700

    Typing indicator (#59)

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -1,9 +1,9 @@
 class ConversationChannel < ApplicationCable::Channel
   def subscribed
-    conversation = Conversation.find_by(id: params[:conversation_id])
+    @conversation = Conversation.find_by(id: params[:conversation_id])
     
-    if conversation && conversation.users.include?(current_user)
-      stream_from "conversation_#{conversation.id}"
+    if @conversation && @conversation.users.include?(current_user)
+      stream_from "conversation_#{@conversation.id}"
     else
       reject
     end
@@ -11,5 +11,18 @@ class ConversationChannel < ApplicationCable::Channel
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def typing
+    return unless @conversation
+
+    ActionCable.server.broadcast(
+      "conversation_#{@conversation.id}",
+      {
+        type: 'typing',
+        user_id: current_user.id,
+        user_name: current_user.full_name
+      }
+    )
   end
 end
